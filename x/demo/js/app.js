d1f5fc46474c9755e63044382a4f34f23005aa76
Pietro <pietro.coretto@gmail.com>
2023-07-06 09:58:13 +0200

    p:regular update

--- x/demo/js/app.js
+++
@@ -1,9 +0,0 @@
-var initializeFileUpload = function() {
-	var fileUpload = document.querySelector('.js_fileupload');
-
-	new FormFileUpload(fileUpload, {
-		// your options are going here
-	});
-};
-
-document.addEventListener("DOMContentLoaded", initializeFileUpload);
